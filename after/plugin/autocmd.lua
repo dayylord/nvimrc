23de194ca3f4b3dea2a4f137f2f7f417cfee97cf
dinesh-21-gs <dineshdd8328@gmail.com>
2023-09-01 20:43:38 +0530

    dumb replace function

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -1,3 +1,4 @@
  vim.cmd([[autocmd BufNewFile *.cpp 0r ~/.config/nvim/skeleton/default.cpp]])
  vim.cmd([[autocmd BufNewFile *.html 0r ~/.config/nvim/skeleton/default.html]])
- vim.cmd([[autocmd BufNewFile *.js :lua JsLoader() ]])
+ vim.cmd([[autocmd BufNewFile *.js :lua M.JsLoader() ]])
+ vim.cmd([[autocmd BufNewFile *.rn :lua M.replace() ]])
--- lua/dinesh/dependency.lua
+++ lua/dinesh/dependency.lua
@@ -1,4 +1,5 @@
-JsLoader = function ()
+M = {}
+M.JsLoader = function ()
     local i = vim.fn.input("hey do you wanna load a custom file? ", "")
     if(i == "y") then
         return "0r ~/.config/nvim/skeleton/default.js"
@@ -7,4 +8,12 @@ JsLoader = function ()
     end
 end
 
-return JsLoader
+
+M.replace = function()
+    local old = vim.fn.input("old: ", "\n")
+    local new = vim.fn.input("new: ", "\n")
+    local laine = "%s/" .. old .. "/" .. new .. "wq"
+    return laine
+end
+
+return M
